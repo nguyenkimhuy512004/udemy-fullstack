edd7b96f96f71b0b1f940fcbefbf88eeefc3a147
nguyenkimhuy512004 <nguyenkimhuy512004@gmail.com>
2026-08-19 18:11:16 +0700

    update code

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const path = require('path');
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
+const connection = require('./config/database');
 
-const mysql = require('mysql2');
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -16,19 +16,12 @@ configViewEngine(app);
 app.use('/', webRoutes);
 
 //test connection
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3307,
-    user: 'root',
-    password: '123456',
-    database: "hoikimhuy",
-});
+
 
 connection.query(
     'select * From  Users u',
     function (err, results, fields) {
         console.log(">>>results= ", results);
-        console.log(">>>fields= ", fields);
     }
 );
 
